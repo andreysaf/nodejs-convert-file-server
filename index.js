6fccb55a41bfaabd52a3d219d102319f8b966e9e
Andrey <a.so777@live.ca>
2020-04-08 16:38:33 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const port = 9000;
+
+// maps file extention to MIME types
+// full list can be found here: https://www.freeformatter.com/mime-types-list.html
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.zip': 'application/zip',
+  '.doc': 'application/msword',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.ttf': 'application/x-font-ttf',
+};
+
+http
+  .createServer(function (req, res) {
+    console.log(`${req.method} ${req.url}`);
+
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+    const sanitizePath = path
+      .normalize(parsedUrl.pathname)
+      .replace(/^(\.\.[\/\\])+/, '');
+    let pathname = path.join(__dirname, sanitizePath);
+
+    fs.exists(pathname, function (exist) {
+      if (!exist) {
+        // if the file is not found, return 404
+        res.statusCode = 404;
+        res.end(`File ${pathname} not found!`);
+        return;
+      }
+
+      // if is a directory return the list of files
+      if (fs.statSync(pathname).isDirectory()) {
+        fs.readdir(pathname, function (err, files) {
+          //handling error
+          if (err) {
+            return console.log('Unable to scan directory: ' + err);
+          }
+          
+          res.setHeader('Content-type', mimeType['.json'])
+          res.end(JSON.stringify(files));
+        });
+      }
+
+      // read file from file system
+      fs.readFile(pathname, function (err, data) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}.`);
+        } else {
+          // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+          const ext = path.parse(pathname).ext;
+          // if the file is found, set Content-type and send data
+          res.setHeader('Content-type', mimeType[ext] || 'text/plain');
+          res.end(data);
+        }
+      });
+    });
+  })
+  .listen(parseInt(port));
+
+console.log(`Server listening on port ${port}`);
