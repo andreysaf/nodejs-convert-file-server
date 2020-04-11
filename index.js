640ab3a135b572c80cdf597f2ff66a1bb4a6cc01
Andrey <a.so777@live.ca>
2020-04-11 09:03:23 -0700

    Added Optimizer by PDFTron

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { PDFNet } = require('@pdftron/pdfnet-node');
 const mimeType = require('./modules/mimeType');
 const port = 9000;
 
@@ -16,14 +17,69 @@ app.get('/files', (req, res) => {
   });
 });
 
-app.get('/files/:fileName', (req, res) => {
-  const pathname = `./files/${req.params.fileName}`;
+app.get('/optimize/:filename', (req, res) => {
+  const pathname = './files/';
+  const filename = req.params.filename;
+
+  const main = async () => {
+    const doc = await PDFNet.PDFDoc.createFromFilePath(pathname+filename);
+    await doc.initSecurityHandler();
+  
+    // compress
+    const image_settings = new PDFNet.Optimizer.ImageSettings();
+  
+    image_settings.setCompressionMode(
+      PDFNet.Optimizer.ImageSettings.CompressionMode.e_jpeg,
+    );
+  
+    const opt_settings = new PDFNet.Optimizer.OptimizerSettings();
+    opt_settings.setColorImageSettings(image_settings);
+    opt_settings.setGrayscaleImageSettings(image_settings);
+  
+    await PDFNet.Optimizer.optimize(doc, opt_settings);
+  
+    // flattener
+    const fl = await PDFNet.Flattener.create();
+    await fl.process(doc, PDFNet.Flattener.Mode.e_fast);
+  
+    // viewer optimizer
+    const opts = new PDFNet.PDFDoc.ViewerOptimizedOptions();
+    opts.setThumbnailRenderingThreshold(0);
+  
+    await doc.saveViewerOptimized(`${pathname}optimized_${filename}`, opts);
+  };
+  
+  // add your own license key as the second parameter, e.g. PDFNet.runWithCleanup(main, 'YOUR_LICENSE_KEY')
+  PDFNet.runWithCleanup(main)
+    .catch(function (error) {
+      console.log('Error: ' + JSON.stringify(error));
+      res.statusCode = 500;
+      res.end(`Error : ${JSON.stringify(error)}.`);
+    })
+    .then(function () {
+      PDFNet.shutdown();
+      const newpath = `${pathname}optimized_${filename}`;
+      fs.readFile(newpath, function (err, data) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}.`);
+        } else {
+          const ext = path.parse(newpath).ext;
+          // if the file is found, set Content-type and send data
+          res.setHeader('Content-type', mimeType[ext] || 'text/plain');
+          res.end(data);
+        }
+      });
+    });
+});
+
+app.get('/files/:filename', (req, res) => {
+  const pathname = `./files/${req.params.filename}`;
   fs.readFile(pathname, function (err, data) {
     if (err) {
       res.statusCode = 500;
       res.end(`Error getting the file: ${err}.`);
     } else {
-      // based on the URL path, extract the file extention. e.g. .js, .doc, ...
       const ext = path.parse(pathname).ext;
       // if the file is found, set Content-type and send data
       res.setHeader('Content-type', mimeType[ext] || 'text/plain');
