fae50baff5ab114b3d36c43b58bec02915bc5a69
Andrey <a.so777@live.ca>
2020-04-10 20:12:46 -0700

    Refactored code into modules.

--- index.js
+++ index.js
@@ -2,27 +2,8 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const mimeType = require('./modules/mimeType');
 const port = 9000;
-
-// maps file extention to MIME types
-// full list can be found here: https://www.freeformatter.com/mime-types-list.html
-const mimeType = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.wav': 'audio/wav',
-  '.mp3': 'audio/mpeg',
-  '.svg': 'image/svg+xml',
-  '.pdf': 'application/pdf',
-  '.zip': 'application/zip',
-  '.doc': 'application/msword',
-  '.eot': 'application/vnd.ms-fontobject',
-  '.ttf': 'application/x-font-ttf',
-};
 
 http
   .createServer(function (req, res) {
---
+++ modules/mimeType.js
@@ -0,0 +1,19 @@
+// maps file extention to MIME types
+// full list can be found here: https://www.freeformatter.com/mime-types-list.html
+module.exports = mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.zip': 'application/zip',
+    '.doc': 'application/msword',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.ttf': 'application/x-font-ttf',
+  };
