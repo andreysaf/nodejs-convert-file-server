9b21aeade87964ba72014cc1dd5ab3b802a6a38e
Andrey <a.so777@live.ca>
2020-04-26 08:44:47 -0700

    First test is now working

--- test/test.js
+++ test/test.js
@@ -1,21 +1,20 @@
 process.env.NODE_ENV = 'test';
 
-const { expect }  = require('chai');
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index.js');
 
 chai.use(chaiHttp);
 
-
 describe('/GET files', () => {
     it('it should GET files', (done) => {
       chai.request(server)
-          .get('/book')
+          .get('/files')
           .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(0);
+            chai.expect(res.status).to.equal(200);
+            chai.expect(res.body).to.be.an('array');
             done();
           });
     });
 });
+
