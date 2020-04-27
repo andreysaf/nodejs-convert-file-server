bf997b3cf26f620a910a75849537add07942bcba
Andrey <a.so777@live.ca>
2020-04-26 19:16:36 -0700

    Added tests for /files and /files/filename

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index.js');
+const mimeType = require('../modules/mimeType');
 
 chai.use(chaiHttp);
 
@@ -13,6 +14,17 @@ describe('/GET files', () => {
           .end((err, res) => {
             chai.expect(res.status).to.equal(200);
             chai.expect(res.body).to.be.an('array');
+            chai.expect(res.type).to.equal(mimeType['.json']);
+            done();
+          });
+    });
+
+    it('it should GET webviewer.pdf', (done) => {
+      chai.request(server)
+          .get('/files/webviewer.pdf')
+          .end((err, res) => {
+            chai.expect(res.status).to.equal(200);
+            chai.expect(res.type).to.equal(mimeType['.pdf']);
             done();
           });
     });
